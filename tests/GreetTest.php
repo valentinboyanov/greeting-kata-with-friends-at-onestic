33ed7d3bf378e3af8d85b7977f69fcf61edb4774
Valentin Boyanov <valentin@onestic.com>
2018-02-03 13:00:46 +0100

    Refactor

--- tests/GreetTest.php
+++ tests/GreetTest.php
@@ -2,41 +2,44 @@
 
 namespace Kata\Tests;
 
-use Kata\Greeter;
+use Kata\Greeter as Greeter;
 use PHPUnit\Framework\TestCase;
 
 class GreetTest extends TestCase
 {
+    /**
+     * @var Greeter
+     */
+    private $greeter;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->greeter = new Greeter();
+    }
+
     public function test_1_greet_class()
     {
-        $this->assertInstanceOf(Greeter::class, (new Greeter()));
+        $this->assertInstanceOf(Greeter::class, $this->greeter);
     }
 
     public function test_2_greet()
     {
-        $greeter = new Greeter();
-
-        $this->assertEquals("Hello, Bob.", $greeter->greet("Bob"));
+        $this->assertEquals("Hello, Bob.", $this->greeter->greet("Bob"));
     }
 
     public function test_3_can_say_hello_to_unknown()
     {
-        $greeter = new Greeter();
-
-        $this->assertEquals("Hello, my friend.", $greeter->greet(null));
+        $this->assertEquals("Hello, my friend.", $this->greeter->greet(null));
     }
 
     public function test_4_handle_shouting()
     {
-        $greeter = new Greeter();
-
-        $this->assertEquals("HELLO PACO!", $greeter->greet('PACO'));
+        $this->assertEquals("HELLO PACO!", $this->greeter->greet('PACO'));
     }
 
     public function test_5_handle_two_names()
     {
-        $greeter = new Greeter();
-
-        $this->assertEquals('Hello, Paco and Paca.', $greeter->greet(['Paco', 'Paca']));
+        $this->assertEquals('Hello, Paco and Paca.', $this->greeter->greet(['Paco', 'Paca']));
     }
 }
